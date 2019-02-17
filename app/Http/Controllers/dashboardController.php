e2be44b167dfefc824b8e71d458b1b0f34647499
mahtanik <nikoufarmahta@gmail.com>
2019-02-17 11:57:14 +0330

    final except users page

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -10,31 +10,37 @@ class dashboardController extends Controller
     public function counter()
     {
         //Summary of the data and changes in all services
-        $autocharges1 = DB::table('transactions_1513991762415000038187743')->where('action' , 'AUTO_CHARGE')->count();
-        $autocharges2 = DB::table('transactions_1513991762415000000015575')->where('action' , 'AUTO_CHARGE')->count();
+        $autocharges1 = DB::table('transactions_1513991762415000038187743')->where('action', 'AUTO_CHARGE')->count();
+        $autocharges2 = DB::table('transactions_1513991762415000000015575')->where('action', 'AUTO_CHARGE')->count();
         $autocharges = $autocharges1 + $autocharges2;
 
-        $unsubs1 = DB::table('transactions_1513991762415000038187743')->where('action' , 'UNSUBSCRIPTION')->count();
-        $unsubs2 = DB::table('transactions_1513991762415000000015575')->where('action' , 'UNSUBSCRIPTION')->count();
+        $unsubs1 = DB::table('transactions_1513991762415000038187743')->where('action', 'UNSUBSCRIPTION')->count();
+        $unsubs2 = DB::table('transactions_1513991762415000000015575')->where('action', 'UNSUBSCRIPTION')->count();
         $unsubs = $unsubs1 + $unsubs2;
-        $subs = DB::table('transactions_1513991762415000038187743')->where('action' , 'SUBSCRIPTION')->count();
+
+        $subs = DB::table('transactions_1513991762415000038187743')->where('action', 'SUBSCRIPTION')->count();
 
         $total1 = DB::table('transactions_1513991762415000038187743')->count();
         $total2 = DB::table('transactions_1513991762415000000015575')->count();
         $total = $total1 + $total2;
 
-        $totalcharge1 = DB::table('transactions_1513991762415000038187743')->where( 'action' , 'AUTO_CHARGE')->orwhere( 'action' , 'SUBSCRIPTION')->count();
-        $totalcharge2 = DB::table('transactions_1513991762415000000015575')->where( 'action' , 'AUTO_CHARGE')->orwhere( 'action' , 'SUBSCRIPTION')->count();
+        $totalcharge1 = DB::table('transactions_1513991762415000038187743')->where('action', 'AUTO_CHARGE')->orwhere('action', 'SUBSCRIPTION')->count();
+        $totalcharge2 = DB::table('transactions_1513991762415000000015575')->where('action', 'AUTO_CHARGE')->orwhere('action', 'SUBSCRIPTION')->count();
         $totalcharge = $total + $total2;
 
         // calcularing the top four services to dispaly in donut-chart
-        $topfour = DB::table( 'transactions_1513991762415000000015575')->select('id')->groupBy('id')->offset(0)->limit(10)->get();
+        $topfour = DB::select('SELECT count(*)  as value, service_name as name FROM `tb_transactions` join service_details2 on  service_details2.service_id =  tb_transactions.serviceId where action="SUBSCRIPTION" or action="AUTO_CHARGE" GROUP by service_name ORDER by COUNT(*) DESC ');
 
+        //bar chart -- total users and total charges for each month
+        $months = array('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12');
+        $total_users = array();
+        $total_charges = array();
 
-        //$servicesShare = DB::table('shareofservices')->orderBy('transactions')->pluck('service_id');
+        foreach ($months as $month) {
+            $total_users[$month] = DB::table('tb_transactions')->where('action' , 'SUBSCRIPTION')->orWhere('action' , 'AUTO_CHARGE')->where('CreatedOn' ,'like' ,  "2018-$month%")->count();
+            $total_charges[$month] = DB::table('tb_transactions')->where('action' , 'AUTO_CHARGE')->where('CreatedOn' ,'like' ,  "2018-$month%")->count();
+    }
 
-       // $servicesShare->transactions = DB::table('tb_transactions')->where('id',$servicesShare)->count();
-
-        return view('dashboard' , [  'autocharges' => $autocharges , 'unsubs' => $unsubs , 'subs'=>$subs , 'total'=> $total , 'totalcharge'=>$totalcharge , 'topfour'=> $topfour]);
+        return view('dashboard' , ['total_charges'=>$total_charges  , 'total_users'=>$total_users , 'autocharges' => $autocharges , 'unsubs' => $unsubs , 'subs'=>$subs , 'total'=> $total , 'totalcharge'=>$totalcharge , 'topfour'=> $topfour]);
     }
 }
